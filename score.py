a3cb3fe00306b124570def339c30197fd8b66a8a
baharenouriyan <bahar.coder3@gmail.com>
2025-12-26 00:23:07 +0330

    گرفتن نمره شرطی و تعییرن سطح ان

---
+++ score.py
@@ -0,0 +1,12 @@
+
+ #گرفتن نمره چند شرطی
+
+num1 = int (input("enter num1:"))
+if num1 >= 90 :
+    print("A")
+elif num1 >= 70 and num1 <= 89 :
+    print("B")
+elif num1 >= 50 and num1 <= 69 :
+ print("C")
+else :
+    print("F")  
